e375022e71629aea23c4a2dbff46fab8cd3f627a
momom88 <zamarskimartin@gmail.com>
2019-03-04 15:48:30 +0100

    connect signin to database

--- server.js
+++ server.js
@@ -18,44 +18,33 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-const databese = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joindes: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joindes: new Date()
-        }
-    ]
-}
 
 app.get('/', (req, res) => {
     res.send(databese.users);
 })
 
 app.post('/signin', (req, res) => {
-    if (req.body.email === databese.users[0].email &&
-        req.body.password === databese.users[0].password) {
-        res.json(databese.users[0]);
-    } else {
-        res.status(400).json('error logging in');
-    }
+    db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if (isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json('unable to get user'))
+            } else {
+                res.status(400).json('wrong credentials')
+            }
+        })
+        .catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
     const hash = bcrypt.hashSync(password);
-
     db.transaction(trx => {
         trx.insert({
             hash: hash,
